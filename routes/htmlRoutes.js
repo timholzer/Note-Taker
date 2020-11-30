d51b3f91a64fa5d262781bbc67964ac77556c6b2
Tim Holzer <timiholzer@gmail.com>
2020-11-29 20:46:29 -0700

    fixed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
   
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/css/styles.css"));
+    res.sendFile(path.join(__dirname, "../assets/css/styles.css"));
 });
   
